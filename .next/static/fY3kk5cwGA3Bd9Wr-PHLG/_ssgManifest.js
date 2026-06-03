8de7f48ff24e42528f6b2a5d453a8f7dd9f4d14d
Oscar <oscarpagerup@gmail.com>
2026-06-03 21:00:21 +0200

    Remove static export, use Netlify Next.js Runtime

--- .next/static/fY3kk5cwGA3Bd9Wr-PHLG/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fomraden","\u002F[locale]\u002Fomraden\u002F[area]","\u002F[locale]\u002Fprogram\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-  ...(isProd && { output: 'export' }),
   trailingSlash: true,
   images: { unoptimized: true },
 }
-
 module.exports = nextConfig

28cd25990def62a29898c6129a7eeb29a6ece3fc
Oscar <oscarpagerup@gmail.com>
2026-06-03 20:54:08 +0200

    Fix Netlify publish directory to out

---
+++ out/_next/static/fY3kk5cwGA3Bd9Wr-PHLG/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fomraden","\u002F[locale]\u002Fomraden\u002F[area]","\u002F[locale]\u002Fprogram\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
